6f5be5bc7ee79ff5272e64407a077a3b41e753d8
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2021-07-30 23:35:54 +0800

    test

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,20 +11,20 @@ export class DashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
-      if (matches) {
-        return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
-        ];
-      }
+      // if (matches) {
+      //   return [
+      //     { title: 'Card 1', cols: 1, rows: 1 },
+      //     { title: 'Card 2', cols: 1, rows: 1 },
+      //     { title: 'Card 3', cols: 1, rows: 1 },
+      //     { title: 'Card 4', cols: 1, rows: 1 }
+      //   ];
+      // }
 
       return [
-        { title: 'Card 1', cols: 1, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 1 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 2', cols: 2, rows: 1 },
+        { title: 'Card 3', cols: 2, rows: 1 },
+        { title: 'Card 4', cols: 2, rows: 1 }
       ];
     })
   );
