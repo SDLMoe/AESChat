951f4d0c08a7a7414e4d7e57133c1c3fdd4a7e6c
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2021-07-30 19:30:27 +0800

    optimize

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,9 +21,9 @@ export class DashboardComponent {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
+        { title: 'Card 1', cols: 1, rows: 1 },
         { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
+        { title: 'Card 3', cols: 1, rows: 1 },
         { title: 'Card 4', cols: 1, rows: 1 }
       ];
     })
