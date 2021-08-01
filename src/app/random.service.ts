90ab93a2fe60a0a744efd931e3cd218f475b50f9
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2021-08-01 19:13:43 +0800

    test

--- src/app/random.service.ts
+++ src/app/random.service.ts
@@ -4,17 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RandomService {
-
-  constructor() { }
-
   dec2hex (dec: { toString: (arg0: number) => string; }) {
-    return dec.toString(36).padStart(2, "0")
+    return dec.toString(36).padStart(8, "0")
   }
 
   generateRandomString (len: number) {
-    let arr = new Uint8Array(len/2)
+    let arr = new Uint32Array(len*2)
     window.crypto.getRandomValues(arr)
     return Array.from(arr, this.dec2hex).join('')
   }
-
+  constructor() { }
 }
