139e1013e1a364979837c9d336ba7093e617ab1d
z8kh8E6t rEv62qT7 <23183218+z8kh8E6t-rEv62qT7@users.noreply.github.com>
2021-08-04 22:13:28 +0800

    build time

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -9,17 +8,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AboutComponent implements OnInit {
 
-  // public getVersion(): Observable<any> {
-  //   let json = this.http.get('./ngsw.json');
-  //   let obj = JSON.parse(json);
-  //   let version = obj.timestamp
-  //   console.log(version)
-  //   return json
-  // }
-
   constructor(private http: HttpClient) {
   }
   ngOnInit() {
   }
-
+  
 }
